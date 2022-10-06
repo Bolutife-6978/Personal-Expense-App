79088c39583b91dee998f4af1b8a473c1d3c71d0
Babatunde Ahmed <bahmed@live.com>
2022-10-06 20:48:25 +0100

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter App'),
+        ),
+        body: Column(
+          children: const <Widget>[
+            Card(
+              child: Text('CHART'),
+            ),
+            Card(
+              child: Text('List of transactions'),
+            )
+          ],
+        ));
+  }
+}
